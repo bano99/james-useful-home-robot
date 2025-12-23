8c18ec81af2a8a1f9838038570ca597456271ccc
Denis <denis@banovic.com>
2025-12-23 10:58:12 +0100

    adopting changes for remote control

--- ros2_ws/src/james_manipulation/james_manipulation/arm_cartesian_controller.py
+++ ros2_ws/src/james_manipulation/james_manipulation/arm_cartesian_controller.py
@@ -161,7 +161,7 @@ class ArmCartesianController(Node):
                 
                 # Calculate velocity commands
                 self.pending_v_x = -joy_lx * self.velocity_scale # Swapped: Joy Left/Right -> Vx (Turn/Base?)
-                self.pending_v_y = -joy_ly * self.velocity_scale # Swapped: Joy Fwd/Back -> Vy (Reach/J2?)
+                self.pending_v_y = joy_ly * self.velocity_scale # Swapped & Inverted: Joy Fwd/Back -> Vy
                 
                 if switch_mode == 'vertical':
                     self.pending_v_z += joy_ry * self.velocity_scale
