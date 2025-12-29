f40a2a4e2baa43f2c43ab5ca1fda72655f4fb56f
Denis <denis@banovic.com>
2025-12-29 12:57:18 +0100

    continuous debugging

--- ros2_ws/src/james_manipulation/james_manipulation/arm_cartesian_controller.py
+++ ros2_ws/src/james_manipulation/james_manipulation/arm_cartesian_controller.py
@@ -222,8 +222,8 @@ class ArmCartesianController(Node):
 
                 self.is_active = True
                 self.stop_sent = False # Reset stop guard
-                self.pending_v_x = joy_lx    # Stick Right -> Robot +X (Side)
-                self.pending_v_y = joy_ly    # Stick Forward -> Robot +Y (Forward)
+                self.pending_v_x = -joy_lx   # Stick Left -> Robot +Y (Wait, this was my confusion)
+                self.pending_v_y = joy_ly    # Stick Forward -> Physical Extension
                 
                 if switch_mode == 'vertical':
                     self.pending_v_z = joy_ry
