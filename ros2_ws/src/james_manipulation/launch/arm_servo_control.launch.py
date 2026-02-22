7a3a7c0fbaccfe0d26b33df48d030111225c7545
Denis <denis@banovic.com>
2026-02-22 13:48:50 +0100

    debugging

--- ros2_ws/src/james_manipulation/launch/arm_servo_control.launch.py
+++ ros2_ws/src/james_manipulation/launch/arm_servo_control.launch.py
@@ -90,7 +90,7 @@ def generate_launch_description():
         composable_node_descriptions=[
             ComposableNode(
                 package='moveit_servo',
-                plugin='moveit_servo::ServoServer',
+                plugin='moveit_servo::ServoNode',
                 name='servo_server',
                 parameters=[
                     servo_yaml,
